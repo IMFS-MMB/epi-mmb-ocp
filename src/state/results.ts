0b88ef0b3ae625d5ff58b4fc9bfebf35ce428736
Jan Lohage <github@j2L4e.de>
2022-05-16 16:44:58 +0200

    trim model names when loading result files

--- src/state/results.ts
+++ src/state/results.ts
@@ -1,13 +1,14 @@
 const files = import.meta.glob('../results/*.json')
 
 export const loadResult = async (model: string, shock: string) => {
-  const name = `../results/${model}-${shock}.output.json`;
+  const name = `../results/${model.trim()}-${shock}.output.json`;
 
   if (files[name]) {
     const { data } = await files[name]();
 
     return data;
   } else {
+    console.warn(`Results file ${name} not found`);
     return null;
   }
 }

1ade039f2daf3d03ad640e2da78266dbca54b1d8
Jan Lohage <github@j2L4e.de>
2022-06-08 12:47:35 +0200

    fix overflowing panels

--- src/components/Ocp.vue
+++ src/components/Ocp.vue
@@ -13,14 +13,14 @@ const { hasSelection } = useState();
 
 <template>
   <div class="h-screen overflow-hidden p-8 grid grid-cols-12 grid-rows-1 gap-8">
-    <Panel class="col-span-3">
+    <Panel class="col-span-3 min-h-0">
       <Models></Models>
     </Panel>
 
     <div class="col-span-2 grid grid-cols-1 gap-8">
-      <Panel><Shocks /></Panel>
-      <Panel><Variables /></Panel>
-      <Panel><Settings /></Panel>
+      <Panel class="min-h-0"><Shocks /></Panel>
+      <Panel class="min-h-0"><Variables /></Panel>
+      <Panel class="min-h-0"><Settings /></Panel>
     </div>
 
     <Panel class="col-span-7 min-h-0 col-span-2 p-0">

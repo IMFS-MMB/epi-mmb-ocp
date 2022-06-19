33853c233bce2a0085b5d24baed5d4d719d21c6c
Jan Lohage <github@j2L4e.de>
2022-06-19 15:29:12 +0200

    Shocks: Show initial infections in popover

--- src/components/Shocks.vue
+++ src/components/Shocks.vue
@@ -4,8 +4,15 @@ import Counter from "./Counter.vue";
 import ClearButton from "./ClearButton.vue";
 import List from "./List.vue";
 import SelectAllButton from "./SelectAllButton.vue";
+import Popper from "vue3-popper";
 
 const { shocks, selectedShocks, visibleShocks } = useState();
+
+const initialInfections = {
+  "Low Initial Infections": ".005%",
+  "Medium Initial Infections": ".01%",
+  "High Initial Infections": ".025%",
+};
 </script>
 <template>
   <List class="shock-list">
@@ -20,18 +27,32 @@ const { shocks, selectedShocks, visibleShocks } = useState();
       </div>
     </template>
 
-    <label
-      class="shock list-item"
-      v-for="shock of visibleShocks"
-      :key="shock.name"
-    >
-      <input
-        class="text-ocp-500 focus:ring-ocp-500"
-        type="checkbox"
-        :value="shock"
-        v-model="selectedShocks"
-      />
-      <span>{{ shock.name }}</span>
-    </label>
+    <template v-for="shock of visibleShocks" :key="shock.name">
+      <Popper
+        placement="bottom"
+        :hover="true"
+        :openDelay="1000"
+        offsetDistance="0"
+        :disabled="!initialInfections[shock.name]"
+      >
+        <label class="shock list-item">
+          <input
+            class="text-ocp-500 focus:ring-ocp-500"
+            type="checkbox"
+            :value="shock"
+            v-model="selectedShocks"
+          />
+          <span>{{ shock.name }}</span>
+        </label>
+        <template #content>
+          <div
+            class="space-y-2 bg-white shadow-lg rounded-lg m-4 p-4 border border-1 border-gray-50 max-w-xs sm:max-w-md lg:max-w-lg xl:max-w-xl"
+          >
+            {{ initialInfections[shock.name] }} of the population are infected
+            initially.
+          </div>
+        </template>
+      </Popper>
+    </template>
   </List>
 </template>
--- src/state/shock.ts
+++ src/state/shock.ts
@@ -1,3 +1,4 @@
 export interface Shock {
   name: string;
+  initialInfections: string;
 }

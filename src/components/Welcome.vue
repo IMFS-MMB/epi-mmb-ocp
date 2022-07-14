d9d237729891fbb1050854f35a99c6666cad9de2
Jan Lohage <github@j2L4e.de>
2022-07-14 18:36:06 +0200

    Scrollable welcome screen

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -5,13 +5,13 @@ const { content } = useState();
 </script>
 
 <template>
-  <div class="welcome flex-grow self-stretch relative box-border">
+  <div class="welcome flex flex-grow self-stretch relative box-border overflow-auto">
     <div
-      class="absolute w-full h-full flex justify-center items-center box-border select-none"
+      class="absolute right-0 top-0 bottom-0 left-0 flex justify-center items-center box-border select-none"
     >
       <img src="/logo.svg" class="w-1/2 opacity-50" />
     </div>
 
-    <div class="bg-transparent relative z-1 prose !max-w-full" v-html="content"></div>
+    <div class="bg-transparent flex-grow relative z-1 prose !max-w-full overflow-auto" v-html="content"></div>
   </div>
 </template>
